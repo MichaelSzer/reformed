cf45c2e1bbf00edfbde61010bdec2e630f271fb1
Michael Szerman <michaelszer@gmail.com>
2019-07-28 14:21:03 -0300

    First commit

---
+++ app/screens/keySelection/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, TextInput } from 'react-native';
+
+import styles from 'styles.js';
+
+class KeySelectionScreen extends Component {
+    
+    state = {
+        textKey: ''
+    }
+
+    render(){
+        <View style={styles.mainView}>
+            
+        </View>
+    }
+}
